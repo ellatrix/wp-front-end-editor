ellatrix/wp-front-end-editor

commit 2a3d6c7d649d299137632f3e5a9a2160837bea64

Fix toolbar position for themes that have a relative positioned body...

js/tinymce.theme.js
@@ -221,7 +221,7 @@ tinymce.ThemeManager.add( 'fee', function( editor ) {
 	editor.toolbarItems = toolbarItems;
 
 	self.renderUI = function() {
-		var panel, toolbars = {}, hasPlaceholder;
+		var panel, toolbars = {}, hasPlaceholder, upperMargin;
 
 		settings.content_editable = true;
 
@@ -283,6 +283,13 @@ tinymce.ThemeManager.add( 'fee', function( editor ) {
 			return {};
 		}
 
+		if ( DOM.getStyle( document.body, 'position', true ) === 'relative' ) {
+			upperMargin =
+				parseInt( DOM.getStyle( document.body, 'margin-top', true ), 10 ) +
+				parseInt( DOM.getStyle( document.documentElement, 'padding-top', true ), 10 ) +
+				parseInt( DOM.getStyle( document.documentElement, 'margin-top', true ), 10 );
+		}
+
 		toolbars.normal = createToolbar( settings.toolbar );
 		toolbars.img = createToolbar( [ 'imgalignleft', 'imgaligncenter', 'imgalignright', 'imgalignnone', 'edit', 'remove' ] );
 		toolbars.view = createToolbar( [ 'editview', 'removeview' ] );
@@ -307,7 +314,7 @@ tinymce.ThemeManager.add( 'fee', function( editor ) {
 				boundaryMiddle = ( boundary.left + boundary.right ) / 2,
 				windowWidth = window.innerWidth,
 				toolbarWidth, toolbarHalf,
-				margin = parseInt( DOM.getStyle( toolbarEl, 'margin-bottom', true ), 10),
+				margin = parseInt( DOM.getStyle( toolbarEl, 'margin-bottom', true ), 10 ) + upperMargin,
 				top, left, className;
 
 			toolbarEl.className = ( ' ' + toolbarEl.className + ' ' ).replace( /\smce-arrow-\S+\s/g, ' ' ).slice( 1, -1 );

commit 757e9613530ea17177c4ea339728b7236f0c7892

Broke something in 2a3d6c7d649d299137632f3e5a9a2160837bea64.

js/tinymce.theme.js
@@ -221,7 +221,8 @@ tinymce.ThemeManager.add( 'fee', function( editor ) {
 	editor.toolbarItems = toolbarItems;
 
 	self.renderUI = function() {
-		var panel, toolbars = {}, hasPlaceholder, upperMargin;
+		var panel, toolbars = {}, hasPlaceholder,
+			upperMargin = 0;
 
 		settings.content_editable = true;
 