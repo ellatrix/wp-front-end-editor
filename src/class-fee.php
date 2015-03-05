e233dd0184a44510f0e3f9f5c7b0c4d56dcfa4b2
iseulde <*@iseulde.com>
2015-03-05 17:07:55 +0000

    Remove the ugly toolbar.

--- src/class-fee.php
+++ src/class-fee.php
@@ -526,9 +526,31 @@ class FEE {
 			$post_type_object->show_in_admin_bar
 		) {
 			$wp_admin_bar->remove_node( 'edit' );
+
 			$wp_admin_bar->add_node( array(
 				'id' => 'edit',
 				'title' => $post_type_object->labels->edit_item,
+				'href' => '#'
+			) );
+
+			$wp_admin_bar->add_node( array(
+				'parent' => 'edit',
+				'id' => 'edit-save',
+				'title' => __( 'Save Draft' ),
+				'href' => '#'
+			) );
+
+			$wp_admin_bar->add_node( array(
+				'parent' => 'edit',
+				'id' => 'edit-publish',
+				'title' => __( 'Publish' ),
+				'href' => '#'
+			) );
+
+			$wp_admin_bar->add_node( array(
+				'parent' => 'edit',
+				'id' => 'edit-cancel',
+				'title' => __( 'Cancel' ),
 				'href' => get_edit_post_link( $current_object->ID )
 			) );
 
@@ -571,96 +593,6 @@ class FEE {
 				<div class="dashicons dashicons-dismiss"></div>
 			</div>
 			<input type="hidden" id="post_ID" name="post_ID" value="<?php echo $post->ID; ?>">
-			<div class="fee-toolbar">
-				<div class="fee-toolbar-right">
-					<?php if ( ! in_array( $post->post_status, array( 'publish', 'future', 'pending' ) ) ) { ?>
-						<button <?php if ( 'private' == $post->post_status ) { ?>style="display:none"<?php } ?> class="button button-large fee-save"><?php _e( 'Save Draft' ); ?></button>
-					<?php } elseif ( 'pending' === $post->post_status && $can_publish ) { ?>
-						<button class="button button-large fee-save"><?php _e( 'Save as Pending' ); ?></button>
-					<?php } ?>
-
-					<div class="button-group">
-						<?php if ( ! in_array( $post->post_status, array( 'publish', 'future', 'private' ) ) || 0 === $post->ID ) { ?>
-							<?php if ( $can_publish ) { ?>
-								<?php if ( ! empty($post->post_date_gmt ) && time() < strtotime( $post->post_date_gmt . ' +0000' ) ) { ?>
-									<button class="button button-primary button-large fee-publish"><?php _e( 'Schedule' ); ?></button>
-								<?php } else { ?>
-									<button class="button button-primary button-large fee-publish"><?php _e( 'Publish' ); ?></button>
-								<?php } ?>
-							<?php } else { ?>
-								<button class="button button-primary button-large fee-publish"><?php _e( 'Submit for Review' ); ?></button>
-							<?php } ?>
-						<?php } else { ?>
-							<button class="button button-primary button-large fee-save"><?php _e( 'Update' ); ?></button>
-						<?php } ?>
-						<button class="button button-primary button-large fee-publish-options" style="padding: 0 2px 2px 1px;">
-							<div class="dashicons dashicons-arrow-down"></div>
-						</button>
-					</div>
-				</div>
-			</div>
-			<div class="fee-publish-options-dropdown">
-				<label for="fee-post-status">
-					<div class="dashicons dashicons-post-status" style="margin-top: 5px;"></div>
-					<select id="fee-post-status">
-						<?php if ( 'publish' === $post->post_status ) { ?>
-							<option<?php selected( $post->post_status, 'publish' ); ?> value="publish"><?php _e( 'Published' ); ?></option>
-						<?php } elseif ( 'private' === $post->post_status ) { ?>
-							<option<?php selected( $post->post_status, 'private' ); ?> value="publish"><?php _e( 'Privately Published' ); ?></option>
-						<?php } elseif ( 'future' === $post->post_status ) { ?>
-							<option<?php selected( $post->post_status, 'future' ); ?> value="future"><?php _e( 'Scheduled' ); ?></option>
-						<?php } ?>
-
-						<option<?php selected( $post->post_status, 'pending' ); ?> value="pending"><?php _e( 'Pending Review' ); ?></option>
-
-						<?php if ( 'auto-draft' === $post->post_status ) { ?>
-							<option<?php selected( $post->post_status, 'auto-draft' ); ?> value="draft"><?php _e( 'Draft' ); ?></option>
-						<?php } else { ?>
-							<option<?php selected( $post->post_status, 'draft' ); ?> value="draft"><?php _e( 'Draft' ); ?></option>
-						<?php } ?>
-					</select>
-				</label>
-
-				<?php if ( $can_publish ) {
-					if ( 'private' === $post->post_status ) {
-						$post->post_password = '';
-						$visibility = 'private';
-					} elseif ( ! empty( $post->post_password ) ) {
-						$visibility = 'password';
-					} elseif ( $post_type == 'post' && is_sticky( $post->ID ) ) {
-						$visibility = 'sticky';
-						$visibility_trans = __( 'Public, Sticky' );
-					} else {
-						$visibility = 'public';
-					}
-				?>
-					<label for="fee-post-visibility">
-						<div class="dashicons dashicons-visibility" style="margin-top: 5px;"></div>
-						<select id="fee-post-visibility">
-							<option<?php selected( $visibility, 'public' ); ?> value="public"><?php _e( 'Public' ); ?></option>
-							<option<?php selected( $visibility, 'sticky' ); ?> value="sticky"><?php _e( 'Public, Sticky' ); ?></option>
-							<option<?php selected( $visibility, 'password' ); ?> value="password"><?php _e( 'Password protected' ); ?></option>
-							<option<?php selected( $visibility, 'private' ); ?> value="private"><?php _e( 'Private' ); ?></option>
-						</select>
-						<div<?php  echo $visibility === 'password' ? '' : ' style="display: none;"'; ?>>
-							<div class="dashicons dashicons-admin-network" style="margin-top: 5px;"></div>
-							<input type="text" id="fee-post-password" value="<?php echo esc_attr( $post->post_password ); ?>"  maxlength="20" />
-						</div>
-					</label>
-				<?php } ?>
-
-				<?php if ( post_type_supports( $post_type, 'author' ) && ( is_super_admin() || current_user_can( $post_type_object->cap->edit_others_posts ) ) ) { ?>
-					<label for="fee-post-author">
-						<div class="dashicons dashicons-admin-users" style="margin-top: 5px;"></div>
-						<?php wp_dropdown_users( array(
-							'who' => 'authors',
-							'name' => 'fee-post-author',
-							'selected' => $post->post_author,
-							'include_selected' => true
-						) ); ?>
-					</label>
-				<?php } ?>
-			</div>
 			<div class="fee-alert fee-leave">
 				<div class="fee-alert-body">
 					<p><?php _e( 'The changes you made will be lost if you navigate away from this page.' ); ?></p>
